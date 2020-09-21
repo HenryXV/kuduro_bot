ab2abc5e8685989dd5b75fe5bb3980fdb0ef72ff
Henrique <henri.r.s@hotmail.com>
2020-09-21 20:55:14 -0300

    added commands: stop, pause and resume now the bot downloads the audios

--- cogs/music.py
+++ cogs/music.py
@@ -8,14 +8,21 @@ class Music(commands.Cog):
 
     @commands.command(name='play', help="Toca o aúdio de vídeos do youtube no canal de voz que o usuário está")
     async def play(self, ctx, url):
+        ydl_opts = {
+            'format': 'bestaudio',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+            'outtmpl': 'song.%(ext)s',
+        }
 
-        ydl_opts = {'format': 'bestaudio', 'noplaylist':'True'}
-        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         channel = ctx.message.author.voice.channel
         voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
 
         if not channel:
-            await ctx.send('Você não está conectado a nenhum canal de voz!')
+            await ctx.send('Você não está conectado a nenhum canal de voz')
         else:
             if voice and voice.is_connected():
                 await voice.move_to(channel)
@@ -23,12 +30,46 @@ class Music(commands.Cog):
                 voice = await channel.connect()
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)
-        URL = info['formats'][0]['url']
-        voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
-        voice.is_playing()
+            info = ydl.extract_info(url, download=True)
 
-        await ctx.send('Tocando: {}'.format(info['title']))
+        if not voice.is_playing():
+            voice.play(discord.FFmpegPCMAudio("song.mp3"))
+            voice.is_playing()
+            await ctx.send('Tocando: {}'.format(info['title']))
+        else:
+            await ctx.send('Um aúdio já está sendo executado')
+            return
+
+    @commands.command(name="stop", help="Interrompe o aúdio sendo executado no momento")
+    async def stop(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        if voice.is_playing():
+            voice.stop()
+            await ctx.send('O aúdio foi interrompido')
+        else:
+            await ctx.send('Não há nenhum aúdio tocando')
+
+    @commands.command(name="pause", help="Pausa o aúdio sendo tocado no momento")
+    async def pause(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        if voice.is_playing():
+            voice.pause()
+            await ctx.send('O aúdio foi pausado')
+        elif voice.is_paused():
+            await ctx.send('O aúdio já está pausado')
+        else:
+            await ctx.send('Não há nenhum aúdio tocando')
+
+    @commands.command(name="resume", help="Resume o aúdio sendo tocado no momento")
+    async def resume(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        if voice.is_paused():
+            voice.resume()
+            await ctx.send('O aúdio foi retomado')
+        elif voice.is_playing():
+            await ctx.send('O aúdio não está pausado')
+        else:
+            await ctx.send('Não há nenhum aúdio tocando')
 
 def setup(bot):
     bot.add_cog(Music(bot))
